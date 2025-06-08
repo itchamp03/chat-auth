2a4fb4fcfea105dcdf66c09840349b0b61cd3000
itchamp03 <yashkapoor09@outlook.com>
2025-06-08 14:00:57 +0200

    login

---
+++ config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://dsmcscvdzytcswsitzbj.supabase.co'; // ← Replace with your URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzbWNzY3Zkenl0Y3N3c2l0emJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNzkzODksImV4cCI6MjA2NDk1NTM4OX0.GaXM7bOg_DBCT-KJljPVIuvD6NhkBzkjMSpPQNlHvIM';               // ← Replace with your anon key
